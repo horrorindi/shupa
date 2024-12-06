da66957d94660143cf5e8b22c2ca4b8e9a2f306a
horrorindi <horrorindigames@gmail.com>
2024-12-06 20:01:49 +0200

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+// Зчитуємо поточне значення лічильника завантажень
+$file = 'download_count.txt';
+if (file_exists($file)) {
+    $count = file_get_contents($file);
+} else {
+    $count = 0;
+}
+
+// Збільшуємо лічильник на 1
+$count++;
+
+// Оновлюємо лічильник у файлі
+file_put_contents($file, $count);
+?>
+
 <!DOCTYPE html>
 <html lang="uk">
 <head>
@@ -34,7 +50,10 @@
 
     <h1>Shupa Horror aka Classroom Escape Horror BETA</h1>
     <p>
-     Версия билда: 041224-88 Тип: beta
+        Версия билда: 041224-88 Тип: beta
+    </p>
+    <p>
+        Кількість завантажень: <?php echo $count; ?>
     </p>
     <a href="rel/build-041224-88.apk" class="btn" download>Android</a>
 
